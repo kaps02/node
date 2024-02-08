e31f0bb50da1e4dec018da7d9555f210773b57c1
kaps02 <kapil02rahate@gmail.com>
2024-02-08 23:23:47 +0530

    adding http module and listen

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(' Kapil Rahate returning ');
+    console.log('Your Name: Kapil Rahate');
+});
+
+server.listen(4000, () => {
+    console.log('Server is running on port 4000');
+});
