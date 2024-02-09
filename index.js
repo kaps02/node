ab93e138618ffbf827101386d8979c87050f84eb
kaps02 <kapil02rahate@gmail.com>
2024-02-09 12:41:29 +0530

    added routing feature based on url

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const http = require('http');
 
+
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(' Kapil Rahate returning ');
-    console.log('Your Name: Kapil Rahate');
-});
+
+const requireUrl = req.url; 
+
+if (requireUrl === '/home'){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(`<h1>welcome to home page!</h1>`);
+} 
+else if (requireUrl === '/about'){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("<h1>About Page</h1>");
+    res.end();
+}
+else if (requireUrl === '/node'){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end("<h1>Nodejs Page</h1>");
+} 
+else {
+    res.writeHead(404);
+    res.end("Error 404: Not Found!");
+}});
+   
 
 server.listen(4000, () => {
     console.log('Server is running on port 4000');
